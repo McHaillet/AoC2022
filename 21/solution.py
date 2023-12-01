8e30e6bc9bfe7b27eb2e2470913e142cc2000fc2
McHaillet <martenchaillet@gmail.com>
2023-12-01 15:47:04 +0100

    day 21 full solution

--- 21/solution.py
+++ 21/solution.py
@@ -1,32 +1,99 @@
 import argparse
 import time
+from operator import add, sub, mul, truediv
+
+
+op_map = {
+    '+': add,
+    '-': sub,
+    '*': mul,
+    '/': truediv,
+}
+op_invert = {
+    '+': sub,
+    '-': add,
+    '*': truediv,
+    '/': mul,
+}
 
 
 def read_file(fname):
-    data = list()
+    data = dict()
     with open(fname, "r") as file:
         for line in file:
             strip = line.strip()
             if strip != "":
-                data.append(strip.replace(':', '='))
+                data[strip.split(':')[0]] = strip.replace(':', '=')
     return data
 
 
+def parse(string):
+    equation = {}
+    equation['y'], func = string.split('=')
+    operator = None
+    for op in op_map.keys():
+        if op in func:
+            operator = op
+    if operator is None:
+        equation['op'] = None
+        equation['xs'] = func.strip()
+    else:
+        equation['op'] = operator
+        equation['xs'] = [x.strip() for x in func.split(operator)]
+    return equation
+
+
+def solve_down(data, start_from='root', break_on=None):
+    if start_from == break_on:
+        raise ValueError
+    equation = parse(data[start_from])
+    if equation['op'] is None:
+        return float(equation['xs'])
+    else:
+        return op_map[equation['op']](
+            solve_down(data, start_from=equation['xs'][0], break_on=break_on),
+            solve_down(data, start_from=equation['xs'][1], break_on=break_on)
+        )
+
+
+def solve_up(data, start_from='humn', final='root', result=0):
+    if start_from == final:
+        return result
+    # there can only be one equation that contains starts_from as a variable
+    equation = parse([x for x in data.values() if start_from in x and not x.startswith(start_from)][0])
+    # get the value of the other variable in this equation
+    value = solve_down(data, start_from=[v for v in equation['xs'] if v != start_from][0])
+    if equation['op'] == '-' and equation['xs'].index(start_from) == 1:
+        f = lambda x: -1 * x
+    elif equation['op'] == '/' and equation['xs'].index(start_from) == 1:
+        f = lambda x: 1 / x
+    else:
+        f = lambda x: x
+    return op_invert[equation['op']](
+        f(solve_up(data, start_from=equation['y'], final=final, result=result)),
+        value
+    )
+
+
 def part_1(data):
-    while len(data) != 0:
-        i = 0
-        while True:
-            try:
-                exec(data[i])
-                data.pop(i)
-                break
-            except NameError:
-                i += 1
-    return int(eval('root'))
+    return int(solve_down(data, start_from='root'))
 
 
 def part_2(data):
-    pass
+    vars = parse(data['root'])['xs']
+
+    try:
+        known, solve = vars
+        result = solve_down(data, start_from=known, break_on='humn')
+    except ValueError:
+        known, solve = vars[1], vars[0]
+        result = solve_down(data, start_from=known, break_on='humn')
+
+    # print('solve =', solve)
+    # print('result =', result)
+
+    solution = solve_up(data, start_from='humn', final=solve, result=result)
+    return int(solution)
 
 
 def main(fname):
